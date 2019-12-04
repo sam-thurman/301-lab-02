3d54f0b60b9ac256878bab1c36b59dfdd5fdf481
Sam Thurman <sam.bbmgmt@gmail.com>
2019-12-04 12:26:23 -0800

    challenge 2 coming rough

--- app.js
+++ app.js
@@ -1,4 +1,5 @@
 const creaturesArray = []
+const keywordArray = []
 
 function Creature(creatureObj) {
   this.image_url = creatureObj.image_url;
@@ -8,6 +9,7 @@ function Creature(creatureObj) {
   this.horns = creatureObj.horns;
 
   creaturesArray.push(this);
+  keywordArray.push(this.keyword);
 }
 
 Creature.prototype.render = function () {
@@ -19,13 +21,34 @@ Creature.prototype.render = function () {
   $newSection.find('h2').text(this.title);
   $newSection.find('img').attr('src', this.image_url);
   $newSection.find('img').attr('alt', this.title);
+  $newSection.find('img').attr('data-keyword', this.keyword);
   $newSection.find('p').text(this.description);
 
   $('main').append($newSection);
+
+  const dropDownMenu = $('#drop-down-option').html();
+  const $newOption = $('<option></option>');
+  $newOption.html(dropDownMenu);
+
+  // $newOption.find('option').attr('value', this.keyword)
+  // $newOption.find('option').text(this.keyword)
+  // $newOption.append($)
+
 };
+
 
 $.get('page-1.json', data => {
   data.forEach(creature => {
     new Creature(creature).render();
+
+    $('#drop-down-menu').append($('<option></option>').attr('value', creature.keyword).text(creature.keyword).attr('id', creature.keyword))
+
   })
+})
+
+
+$('option').on('click', function () {
+  event.preventDefault();
+  $('img').hide();
+  console.log('clicked')
 })
